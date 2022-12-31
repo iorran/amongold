800d3aa64ba01f78b8ce410ee7e8cf0a61aa2d0c
Carlos Schuenck <carlosschuenck@hotmail.com>
2022-12-31 18:20:13 +0000

    Adding route to fetch rooms

--- pages/api/rooms.ts
+++ pages/api/rooms.ts
@@ -1,31 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import clientPromise from "../../config/mongodb";
+import { v4 as uuidv4 } from 'uuid';
 
-import {v4 as uuidv4} from 'uuid';
-
-
-
-type Response = {
-    roomId: string
+type Room = {
+    id: string
 }
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Response>
+    res: NextApiResponse<Room | Room[] | string>
 ) {
+    const client = await clientPromise;
    if(req.method === 'POST') {
-       let roomId: string = uuidv4().substring(0,6);
+       let id: string = uuidv4().substring(0,6);
        try {
-           const client = await clientPromise;
            const db = client.db("amongold");
            const rooms = db.collection("rooms");
-           await rooms.insertOne({
-               id: roomId
-           })
+           await rooms.insertOne({ id })
        } catch (e: any){
-           console.log("error mongo", e)
+           console.log("Error when try to insert in rooms", e)
+           res.status(500).send("Error when try to insert in rooms");
+           return;
        }
-       res.status(200).json({ roomId })
+       res.status(200).json({ id })
+   } else if (req.method === 'GET'){
+       try {
+           const db = client.db("amongold");
+           const rooms = await db
+               .collection("rooms")
+               .find({})
+               .sort({ metacritic: -1 })
+               .limit(50)
+               .toArray();
+           res.status(200)
+              .json(rooms.map((room) => room['id']))
+       } catch (e: any){
+           console.log("Error when searching for rooms", e);
+           res.status(500).send("Error when searching for rooms");
+           return;
+       }
+   } else {
+       res.status(404).json('Path not found')
    }
 }
