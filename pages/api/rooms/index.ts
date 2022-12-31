33fe2d46c25e6d509a51ff09b8e8781c51ab2b68
Iorran Castro <iorranpt@gmail.com>
2022-12-31 21:16:32 +0000

    cors

--- pages/api/rooms/index.ts
+++ pages/api/rooms/index.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../../config/mongodb";
 import { v4 as uuidv4 } from "uuid";
+import { headers } from "../../../utils/api/headers";
 
 export type Room = {
   id: string;
@@ -12,6 +13,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Room | Room[] | string>
 ) {
+  headers(res);
+
   const client = await clientPromise;
   const db = client.db("amongold");
   const { name } = req.body;
---
+++ utils/api/headers.ts
@@ -0,0 +1,13 @@
+import { NextApiResponse } from "next";
+
+export const headers = (res: NextApiResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+};
