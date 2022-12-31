9f2353dcb3306fe7c64060c909abc7298d9e66ce
Iorran Castro <iorranpt@gmail.com>
2022-12-31 21:50:03 +0000

    fix

--- components/match.tsx
+++ components/match.tsx
@@ -1,9 +1,12 @@
+import { api } from "../services/api";
 import { usePlayers } from "../services/usePlayers";
 
 export type MatchProps = {
   name: string;
   room: string;
 };
+
+//TODO: must be a route
 
 export default function Match({ name, room }: MatchProps) {
   const { data } = usePlayers(room as string);
@@ -13,10 +16,24 @@ export default function Match({ name, room }: MatchProps) {
   }
 
   const player = data.joiners.find((player) => player.name === name);
+  const isOwner = player?.name === name;
+
+  async function onStart() {
+    await api.post(`api/game/start?roomId=${room}`);
+  }
 
   return (
     <section>
       {player?.name} voce e: <kbd className="kbd">{player?.role} </kbd>
+      {isOwner ? (
+        <button
+          type="button"
+          onClick={onStart}
+          className="btn btn-primary w-40"
+        >
+          Start
+        </button>
+      ) : null}
     </section>
   );
 }
